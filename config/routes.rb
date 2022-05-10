bbc36a4ec8dfcbcf53c26e12d8570e7df10a8f1a
shun19970310 <shunma19970310@gmail.com>
2022-05-10 12:32:54 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: "homes#top"
+  end
 
+  scope module: :public do
+    root to: "homes#top"
+    get 'homes/about'
+    resource :customers, only: [:show, :edit, :update]
+    get 'customers/unsubscribe'
+    patch 'customers/withdraw'
+    resources :posts do
+     resources :comments, only:[:create, :destroy]
+     resource :likes, only: [:create,:destroy]
+    end
 
-  root to: "home#index"
+    resources :groups, only: [:create, :new, :edit, :update]
+  end
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
