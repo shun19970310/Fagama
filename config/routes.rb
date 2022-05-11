0d04ea6c338ed97996c62d795ad88abe2e927279
‘shun19970310’ <shunma19970310@gmail.com>
2022-05-11 09:12:52 +0000

    ヘッダー・フッター作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
     root to: "homes#top"
   end
@@ -6,7 +16,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get 'home/about' => 'homes#about', as: 'about'
-    resource :customers, only: [:show, :edit, :update]
+    resources :customers, only: [:show, :edit, :update]
     get 'customers/unsubscribe'
     patch 'customers/withdraw'
     resources :posts do
@@ -17,13 +27,6 @@ Rails.application.routes.draw do
     resources :groups, only: [:create, :new, :edit, :update]
   end
 
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
 
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_111756) do
+ActiveRecord::Schema.define(version: 2022_05_11_065412) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 2022_05_10_111756) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
-    t.string "avatar"
     t.boolean "is_deleted", default: false
+    t.string "image_id"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
