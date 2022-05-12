7f0cd9d512dac6b6695c6ea8ab5048bc065bf5bb
‘shun19970310’ <shunma19970310@gmail.com>
2022-05-12 11:17:29 +0000

    いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :customer
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  def liked_by?(customer)
+    likes.where(customer_id: customer.id).exists?
+  end
 end
