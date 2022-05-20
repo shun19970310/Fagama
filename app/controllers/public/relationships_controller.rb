6c4bf10acfff189ceef64b46ff9cbc858660ae25
shun19970310 <shunma19970310@gmail.com>
2022-05-20 14:40:17 +0000

    フォロー機能追加

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,15 @@
+class Public::RelationshipsController < ApplicationController
+  before_action :authenticate_customer!
+
+  def create
+    following = current_customer.relationships.build(follower_id: params[:customer_id])
+    following.save
+    redirect_to request.referrer || root_path
+  end
+
+  def destroy
+    following = current_customer.relationships.build(follower_id: params[:customer_id])
+    following.destroy
+    redirect_to request.referrer || root_path
+  end
+end
