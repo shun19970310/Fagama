94da8972ee59c1ddb32e99634cf0e5a684a8be7e
‘shun19970310’ <shunma19970310@gmail.com>
2022-05-21 10:38:17 +0000

    グループメンバーへのメール送信

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -50,6 +50,18 @@ class Public::GroupsController < ApplicationController
     redirect_to groups_path, notice: 'グループを削除しました。'
   end
 
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_customers = @group.customers
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title, @mail_content,group_customers).deliver
+  end
+
   private
 
   def group_params
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV['KEY'],
+    password:             ENV['SECRET_KEY'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
