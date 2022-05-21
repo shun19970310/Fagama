54dd17035580a97db382ce3a1e9cb58a2a16ac0d
‘shun19970310’ <shunma19970310@gmail.com>
2022-05-21 01:24:33 +0000

    マイページ内のフォローボタン

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,12 +4,12 @@ class Public::RelationshipsController < ApplicationController
   def create
     following = current_customer.relationships.build(follower_id: params[:customer_id])
     following.save
-    redirect_to request.referrer || root_path
+    redirect_to request.referrer
   end
 
   def destroy
     following = current_customer.relationships.build(follower_id: params[:customer_id])
     following.destroy
-    redirect_to request.referrer || root_path
+    redirect_to request.referrer
   end
 end
