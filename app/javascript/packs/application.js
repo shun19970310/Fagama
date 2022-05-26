bff2fa09a684e8a7194350170a0803e084042990
shun19970310 <shunma19970310@gmail.com>
2022-05-26 10:49:01 +0000

    デプロイテスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,17 @@ require('jquery')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+  $(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+  });
+});
