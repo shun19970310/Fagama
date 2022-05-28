45299bb0e6ecbcc19f6280ad711b2115a75968a5
‘shun19970310’ <shunma19970310@gmail.com>
2022-05-28 13:02:23 +0000

    [update]アクセス制限/部分テンプレート

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,15 +1,20 @@
 class Public::LikesController < ApplicationController
  before_action :authenticate_customer!
+ before_action :set_post, only: [:create, :destroy]
 
   def create
-   @post = Post.find(params[:post_id])
    like = current_customer.likes.new(post_id: @post.id)
    like.save
   end
 
   def destroy
-   @post = Post.find(params[:post_id])
    like = current_customer.likes.find_by(post_id: @post.id)
    like.destroy
+  end
+
+  private
+
+  def set_post
+   @post = Post.find(params[:post_id])
   end
 end
