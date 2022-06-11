21596543208ba04aaee0a8c67868abab12b823b0
shun19970310 <shunma19970310@gmail.com>
2022-06-11 05:20:04 +0000

    [Add]投稿一覧/管理者画面

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,23 @@
+class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @posts = Post.all.includes(:customer).order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path, notice: "削除に成功しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category).merge(customer_id: current_customer.id)
+  end
+end
--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,7 +6,7 @@ class Public::CommentsController < ApplicationController
     comment = current_customer.comments.new(comment_params)
     comment.post_id = post.id
     if comment.save
-      redirect_to post_path(post), notice: 'コメントしました'
+      redirect_to post_path(post)
     else
       redirect_to post_path(params[:post_id]), alert: 'コメントを入力して下さい'
     end
@@ -14,7 +14,7 @@ class Public::CommentsController < ApplicationController
 
   def destroy
     Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id]), alert: 'コメントを削除しました'
+    redirect_to post_path(params[:post_id])
   end
 
   private
