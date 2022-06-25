6c7d82128ab3632a3a4e1a8d309dafe5f7fa660b
shun19970310 <shunma19970310@gmail.com>
2022-06-25 04:23:06 +0000

    [Update]ゲストログイン

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -40,7 +40,7 @@ class Customer < ApplicationRecord
   end
 
   def self.guest
-    find_or_create_by!(name: "guestuser" ,email: "guest@example") do |customer|
+    find_or_create_by!(name: "guestuser" ,email: "guest@example.com") do |customer|
       customer.password = SecureRandom.urlsafe_base64
       customer.name = "guestuser"
     end
